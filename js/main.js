b133a2a2db884a4011c62622b6e8dcd13b4c21bc
ippoRulez <ippolitodonato@gmail.com>
2022-11-07 19:58:49 +0100

    changed some stiling and logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 const listaSpesa = ['Mele', 'Pere', 'Pane', 'Uova', 'Pasta', 'Latte'];
 const listEl = document.getElementById('lista');
+const addBtnEl = document.getElementById('addBtn');
+const newItemEl = document.getElementById('newItem');
 
 let i = 0;
 
-while (i < listaSpesa.length) {
-	const listItem = document.createElement('li');
+//racchiudere in una funzione
+function whileLoop() {
+	while (i < listaSpesa.length) {
+		const tableRow = document.createElement('tr');
+		const tableItem = document.createElement('td');
 
-	listItem.innerHTML = listaSpesa[i];
-	listEl.append(listItem);
-	i++;
+		tableItem.innerHTML = listaSpesa[i];
+
+		tableRow.append(tableItem);
+		listEl.append(tableRow);
+		i++;
+	}
 }
+
+addBtnEl.addEventListener('click', function () {
+	const itemToAdd = newItemEl.value;
+
+	if (itemToAdd !== '') {
+		listaSpesa.push(itemToAdd);
+		whileLoop();
+	}
+
+	newItemEl.value = '';
+});
+
+whileLoop();
